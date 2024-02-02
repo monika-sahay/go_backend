8a1e0852c42dfbde47e40d801d72483dd5a53006
monika-sahay <monika_sahay@yahoo.com>
2024-02-02 10:36:36 +0100

    added base.layout.tmpl and updated RenderTemplate

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
